5788c4c8dc3775dce9a27c591f7bc8952aa0c208
peterchic <peterchicarielli@gmail.com>
2017-09-14 23:15:25 -0400

    cleaned code

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,24 +1,16 @@
 class Api::V1::CategoriesController < ApplicationController
 
-
   def index
     @categories = Category.all
     render json: @categories
   end
 
-  # def new
-  #   @category = Category.new
-  # end
-
   def create
-    # binding.pry
-    # binding.pry
     @category = Category.create(title: params[:title])
     render json: @category
   end
 
   def show
-    # binding.pry
     @category = Category.find(params[:id])
     render json: @category
   end
--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -1,30 +1,16 @@
 class Api::V1::TranslationsController < ApplicationController
 
-
   def index
     @translations = Translation.all
     render json: @translations
   end
 
-  # def new
-  #   @translation = Translation.new
-  # end
-
   def create
-    # binding.pry
     @category = Category.find_by(title: params[:title])
     @translation = Translation.create(original: params[:original], modified_sp: params[:modified_sp], category_id: @category.id)
     render json: @translation
   end
-  # modified: params[:inputPri],
 
-  # def show
-  #   @translation = Translation.find(params[:id])
-  #   respond_to do |format|
-  #     format.html { render :show }
-  #     format.json { render json: @translation }
-  #   end
-  # end
 
   # private
   #
